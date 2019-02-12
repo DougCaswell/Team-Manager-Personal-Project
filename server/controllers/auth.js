6c744f20c17efb261e05fa4d8b7e52725fa9638b
DougCaswell <dougcaswell@gmail.com>
2019-02-12 15:28:55 -0700

    changed nodemailer to gmail-send

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,16 +1,7 @@
 const bcrypt = require('bcryptjs');
 const randomString = require('randomstring')
-const nodemailer = require('nodemailer');
 
 const { USER, PASS, REACT_APP_LOGIN } = process.env
-
-const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: USER,
-        pass: PASS
-    },
-});
 
 module.exports = {
 
@@ -48,21 +39,17 @@ module.exports = {
             let hash = bcrypt.hashSync(password, salt);
             let createdUser = await db.create_user([email, hash, confirmationCode])
             req.session.user = createdUser[0]
-            let mailOptions = {
-                from: 'doug.teammanager@gmail.com',
+            let send = require('gmail-send')({
+                user: USER,
+                pass: PASS,
                 to: email,
                 subject: `Welcome`,
                 html: `<h1>Welcome</h1><h2>New User</h2><p>This is Team Manager.  A web application designed to make it easy for team members to connect with each other and keep their teams organized.  <h3>Please note that this application is currently not a permanent product.  It may be changed, updated, or removed at any time.</h3>  To activate your new account, if you are still on the register page, you can enter the confirmation code there.  Otherwise, go to ${REACT_APP_LOGIN} and enter in your account info.  This is a one time process.  You won't need the confirmation code after activating your account.</h3><h3>Confirmation Code: <p>${confirmationCode}</p></h3><p>After successfully logging in, you can update your profile information and password on the profile page if you would like. You can now create a team, or if invited, be able to see your pending team invites and accept or decline them on the invite page. Team managers can create events for their teams, and all team team members can chat with others in their chat rooms.</p><h3>This is a project created by Doug Caswell.  For questions and comments about this project, please send your emails to doug.teammanager@gmail.com</h3>`
-            };
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error)
-                    return res.sendStatus(500)
-                } else {
-                    console.log(info);
-                }
-            });
-            res.status(200).send({ loggedIn: false, message: 'New user successfully created', user: req.session.user })
+            })
+            send({}, function (err, res) {
+                console.log(`res: ${res}; err: ${err}`)
+            })
+            res.status(201).send({ loggedIn: false, message: 'New user successfully created', user: req.session.user })
         }
     },
 
--- server/controllers/invite.js
+++ server/controllers/invite.js
@@ -1,17 +1,8 @@
 require('dotenv').config();
 const randomString = require('randomstring')
-const nodemailer = require('nodemailer');
 const bcrypt = require('bcryptjs')
 
 const { USER, PASS, REACT_APP_LOGIN } = process.env
-
-const transporter = nodemailer.createTransport({
-    service: 'yahoo',
-    auth: {
-        user: USER,
-        pass: PASS
-    },
-});
 
 module.exports = {
     getInvites: async (req, res) => {
@@ -34,21 +25,17 @@ module.exports = {
             let hash = bcrypt.hashSync(presetPassword, salt);
             await db.create_user([email, hash, confirmationCode])
             const newUser = await db.find_user([email])
-            let mailOptions = {
-                from: 'doug.teammanager@gmail.com',
+            let send = require('gmail-send')({
+                user: USER,
+                pass: PASS,
                 to: email,
                 subject: `You have been invited to join ${req.session.team.name}`,
                 html: `<h1>Welcome</h1><h2>New User</h2><p>This is Team Manager.  A web application designed to make it easy for team members to connect with each other and keep their teams organized.  <h3>Please note that this application is currently not a permanent product.  It may be changed, updated, or removed at any time.</h3>  You were invited to join one of our teams. ${req.session.team.name}.  To complete joining this team, go to ${REACT_APP_LOGIN} and enter the following information.</p><h3>Email: <p>${email}</p></h3><h3>Password: <p>${presetPassword}</p></h3><h3>Confirmation Code: <p>${confirmationCode}</p></h3><p>After successfully logging in, you can update your profile information and password on the profile page if you would like.  You will also be able to see your pending team invites and accept or decline them on the invite page.</p><h3>This is a project created by Doug Caswell.  For questions and comments about this project, please send your emails to doug.teammanager@gmail.com</h3>`
-            };
+            })
+            send({}, function (err, res) {
+                console.log(`res: ${res}; err: ${err}`)
+            })
             await db.create_invite([req.session.team.id, req.session.team.name, newUser[0].id])
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    console.log(error)
-                    return res.sendStatus(500)
-                } else {
-                    console.log(info);
-                }
-            });
             return res.status(200).send({ message: 'Invite Sent' })
         } else {
             const onTeam = await db.find_user_on_team([req.session.team.id, user[0].id])
